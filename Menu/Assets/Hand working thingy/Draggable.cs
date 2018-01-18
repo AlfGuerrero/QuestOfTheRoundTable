a3be49751383037992345caa70997206d979cbd6
brandon-knight <brandonknight@carleton.ca>
2018-01-18 10:36:05 -0500

    Menu Skeleton / Card Testing

---
+++ Menu/Assets/Hand working thingy/Draggable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Vector2 dragOffset = new Vector2(0f, 0f);
+
+	public void OnBeginDrag(PointerEventData eventData)    {
+		dragOffset = eventData.position - (Vector2)this.transform.localPosition;
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		this.transform.localPosition = eventData.position - dragOffset;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		
+	}
+
+}
