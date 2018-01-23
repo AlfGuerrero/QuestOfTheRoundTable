4d3a04d7d6f06ab527cbfe3334c0ccb4a4043288
Alfieguerrero <alfrancis.guerrero@carleton.ca>
2018-01-23 12:46:42 -0500

    GameController
    added GameController script

---
+++ Menu/Assets/Hand working thingy/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	int Players_turn = 0; 
+	int Players_amount = 3;
+
+	HorizontalLayoutGroup layout;
+
+	public GameObject Card;
+
+	public List<GameObject> createdObjects = new List<GameObject>();
+
+	// use map.
+	// Use this for initialization
+	void Start () {
+		layout = this.GetComponent<HorizontalLayoutGroup> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Event to cycle through players. 
+		if (Input.GetKeyDown ("space"))
+		{
+			print ("Player: " + Players_turn % Players_amount + " turn.");
+			Players_turn += 1;
+
+			GameObject go = (GameObject)Instantiate(Card, new Vector2(0.0f, 0.0f), Quaternion.identity);
+			go.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform);
+			createdObjects.Add(go);
+
+		}
+	}
+}
