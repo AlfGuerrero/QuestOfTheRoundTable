f5d231362fcea0042f54a0490bf9a49edf3de423
brandon-knight <brandonknight@carleton.ca>
2018-01-25 13:49:41 -0500

    Menu / InGameUI

--- Menu/Assets/Hand working thingy/HandManager.cs
+++ Menu/Assets/Hand working thingy/HandManager.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class HandManager : MonoBehaviour {
 
-	int handSize;
 	HorizontalLayoutGroup layout;
+
+	GameObject [] cardsInHand;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,9 @@ public class HandManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		SetCardsInList ();
 		CardDistribution ();
+
 	}
 
 	void DrawCard(){
@@ -24,13 +27,36 @@ public class HandManager : MonoBehaviour {
 
 	void CardDistribution(){
 
-		handSize = this.transform.childCount;
+		int handSize = this.transform.childCount;
 
-		if (handSize <= 7) {
+
+		if (handSize <= 8) {
 			layout.spacing = 0;
+			//1;
+			//boxCollider.offset = new Vector2(1f,boxCollider.offset.y);
+			ResizeCardColliders(1f);
+		} else if (handSize <= 12) {
+			layout.spacing = -34;
+			//-15;
+			//boxCollider.offset = new Vector2(-15f,boxCollider.offset.y);
+			ResizeCardColliders(-15f);
 		} else {
-			layout.spacing = -41;
+			layout.spacing = -54;
+			//-26
+			//boxCollider.offset = new Vector2(-26f,boxCollider.offset.y);
+			ResizeCardColliders(-26f);
 		}
 
+	}
+
+	void SetCardsInList(){
+		cardsInHand = GameObject.FindGameObjectsWithTag ("Card");
+	}
+
+	void ResizeCardColliders(float offsetX){
+		foreach(GameObject card in cardsInHand){
+			BoxCollider2D col = card.GetComponent<BoxCollider2D> ();
+			col.offset = new Vector2 (offsetX, col.offset.y);
+		}
 	}
 }
--- Menu/Assets/Hand working thingy/MouseOver.cs
+++ Menu/Assets/Hand working thingy/MouseOver.cs
@@ -1,27 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseOver : MonoBehaviour {
 
 	Vector3 startPos;
+	public GameObject blownUpCard;
+	public bool mousingOver = false;
 
 	public void OnMouseEnter()
 	{
-		startPos = this.transform.localPosition;
-		//this.transform.SetAsLastSibling ();
+		//startPos = this.transform.localPosition;
+		blownUpCard = new GameObject("blownUpCard");
+		blownUpCard.AddComponent<SpriteRenderer>();
+		blownUpCard.GetComponent<SpriteRenderer> ().sprite = this.GetComponent<Image> ().sprite;
+
+		blownUpCard.transform.position = new Vector3 (this.transform.position.x,5f,0f);
+		blownUpCard.transform.localScale = new Vector3 (3, 3, 3);
 	}
 
 	public void OnMouseOver()
 	{
-		this.transform.localScale = new Vector3 (2,2,1);
-		this.transform.localPosition = new Vector3 (startPos.x, startPos.y + this.GetComponent<RectTransform>().rect.height / 2, startPos.z);
+		mousingOver = true;
+		//this.transform.localScale = new Vector3 (2,2,1);
+		//this.transform.localPosition = new Vector3 (startPos.x, startPos.y + this.GetComponent<RectTransform>().rect.height / 2, startPos.z);
 	}
 
 	public void OnMouseExit()
 	{
-		this.transform.localScale = new Vector3 (1,1,1);
-		this.transform.localPosition = startPos;
+		mousingOver = false;
+		Destroy (GameObject.Find ("blownUpCard"));
+		//this.transform.localScale = new Vector3 (1,1,1);
+		//this.transform.localPosition = startPos;
 	}
 
 }
