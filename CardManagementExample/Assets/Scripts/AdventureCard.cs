7e63ad0bb8a7cc7e30476caa3844ae75eeddd480
kiwisuh <heewon.suh@cmail.carleton.ca>
2018-01-26 22:17:40 -0500

    Adding various classes

---
+++ CardManagementExample/Assets/Scripts/AdventureCard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using m_ally = Ally;
+public class AdventureCard {
+	protected static readonly string[] ADVENTURE_TYPE = {"Foe", "Ally", "Weapon", "Armour", "Test"};
+	protected string name;
+	protected string type;
+	//protected int battlePoints;
+	//protected int bidPoints;
+	//protected int bonusBattlePoints;
+	//protected int bonusBidPoints;
+	public AdventureCard(){
+		this.type = "";
+		this.name = "";
+	}
+	public AdventureCard(string type, string name){
+		this.type = type;
+		this.name = name;
+			if (type.Equals ("Ally")) {
+				Ally ally = new Ally(name);
+			} else if (type.Equals ("Foe")) {
+			} else if (type.Equals ("Weapon")) {
+			} else if (type.Equals ("Armour")) {
+			}else if (type.Equals ("Test")) {
+			}
+
+	}
+
+	public string getName(){
+		return name;
+	}
+	public string getType(){
+		return type;
+	}
+		
+}
+
