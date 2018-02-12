c088d1585540f12d16615f457eea1d2b96375cef
brandon-knight <brandonknight@carleton.ca>
2018-02-11 22:29:22 -0500

    newlyImplementedCards

---
+++ CardManagementExample/Assets/NewImplementation/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	protected static readonly string[] TEST_NAME = {"Test of the Questing Beast", "Test of the Temptation", "Test of the Valor", "Test of the Morgen Le Fey"};
+	protected string name;
+	protected int bidRequirements;
+	protected int bonusBidRequirements;
+	public Test(string name){
+		this.name = name;
+		if (name.Equals (TEST_NAME [0])) {
+			this.bonusBidRequirements = 4;
+		}else if (name.Equals (TEST_NAME [3])) {
+			this.bidRequirements = 3;
+		}
+	}
+
+	public string getName(){
+		return this.name;
+	}
+	public int getbidRequirements(){
+		return this.bidRequirements;
+	}
+	public int getbonusBidRequirements(){
+		return this.bonusBidRequirements;
+	}
+}
