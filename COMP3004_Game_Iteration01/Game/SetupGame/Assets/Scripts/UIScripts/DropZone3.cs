fdff38a79b6b18c54fdcfb20f8f791fe951bcd23
kiwisuh <heewonsuh@cmail.carleton.ca>
2018-03-02 18:27:48 -0500

    adding discarded cards back into adventure deck

--- COMP3004_Game_Iteration01/Game/SetupGame/Assets/Scripts/UIScripts/DropZone3.cs
+++ COMP3004_Game_Iteration01/Game/SetupGame/Assets/Scripts/UIScripts/DropZone3.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 public class DropZone3 : MonoBehaviour, IDropHandler  {
 
 	public void OnDrop(PointerEventData eventData){
+		GameObject game_manager = GameObject.FindGameObjectWithTag ("GameController");
+		game_manager.GetComponent<GameManager>().advDeck.GetComponent<AdventureDeck>().adventureDeck.Add(eventData.pointerDrag.gameObject.GetComponent<AdventureCard>().getName());
 		Destroy (eventData.pointerDrag.gameObject);
 	}
 
